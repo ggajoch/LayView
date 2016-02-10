61a6fa73808dbfef8af46d7032c11453486cd722
Piotr Rzeszut <piotrva@wp.pl>
2016-02-10 18:35:42 +0100

    Some refactor

--- src/moleculesampleapp/CameraView.java
+++ src/moleculesampleapp/CameraView.java
@@ -1,5 +1,6 @@
 package moleculesampleapp;
 
+import com.sun.javafx.geom.Vec3d;
 import javafx.event.EventHandler;
 import javafx.geometry.Point3D;
 import javafx.scene.Group;
@@ -26,11 +27,9 @@ public class CameraView {
     private double mouseDeltaX;
     private double mouseDeltaY;
 
-    private double xAngle;
-    private double yAngle;
-    private double zAngle;
+    private Vec3d angle;
     private double scale;
-    private double xOffset, yOffset, zOffset;
+    private Vec3d offset;
 
     private static final double SCROLL_SCALE = 0.0025;
     private static final double ROTATE_SCALE = .01;
@@ -51,9 +50,9 @@ public class CameraView {
     private SubScene scene;
 
     CameraView(double width, double height, boolean depthBuffer, SceneAntialiasing sceneAntialiasing){
-        xOffset = yOffset = zOffset = 0;
+        offset = new Vec3d(0,0,0);
         scale = 0;
-        xAngle = yAngle = zAngle = 0;
+        angle = new Vec3d(0,0,0);
 
         xRotate = new Rotate(0,new Point3D(0,0,1));
         yRotate = new Rotate(0,new Point3D(1,0,0));
@@ -100,17 +99,17 @@ public class CameraView {
 
                 if(me.isMiddleButtonDown()){
 
-                    xOffset = yOffset = zOffset = 0;
+                    offset.x = offset.y = offset.z = 0;
                     scale = 0;
-                    xAngle = yAngle = zAngle = 0;
+                    angle.x = angle.y = angle.z = 0;
 
-                    xRotate.setAngle(Math.toDegrees(xAngle));
-                    yRotate.setAngle(Math.toDegrees(yAngle));
-                    zRotate.setAngle(Math.toDegrees(zAngle));
+                    xRotate.setAngle(Math.toDegrees(angle.x));
+                    yRotate.setAngle(Math.toDegrees(angle.y));
+                    zRotate.setAngle(Math.toDegrees(angle.z));
 
-                    xyzTranslate.setX(xOffset);
-                    xyzTranslate.setY(yOffset);
-                    xyzTranslate.setZ(zOffset);
+                    xyzTranslate.setX(offset.x);
+                    xyzTranslate.setY(offset.y);
+                    xyzTranslate.setZ(offset.z);
 
                     cameraScale.setX(Math.pow(10,scale));
                     cameraScale.setY(Math.pow(10,scale));
@@ -137,35 +136,35 @@ public class CameraView {
                 }
 
                 if(me.isPrimaryButtonDown()){
-                    Rotation baseRotation = new Rotation(RotationOrder.XYZ, xAngle, yAngle, zAngle);//get base rotation (it is transform form neutral point to actual point of view)
+                    Rotation baseRotation = new Rotation(RotationOrder.XYZ, angle.x, angle.y, angle.z);//get base rotation (it is transform form neutral point to actual point of view)
                     Rotation deltaRotation = new Rotation(new Vector3D(0,0), new Vector3D(-mouseDeltaX*ROTATE_SCALE,-mouseDeltaY*ROTATE_SCALE));//get rotation according to mouse movement
                     Rotation finalRotation = deltaRotation.applyTo(baseRotation);//sum both the rotations
                     try {
                         double angles[] = finalRotation.getAngles(RotationOrder.XYZ);
 
-                        xAngle = angles[0];
-                        yAngle = angles[1];
-                        zAngle = angles[2];
+                        angle.x = angles[0];
+                        angle.y = angles[1];
+                        angle.z = angles[2];
 
-                        xRotate.setAngle(Math.toDegrees(xAngle));
-                        yRotate.setAngle(Math.toDegrees(yAngle));
-                        zRotate.setAngle(Math.toDegrees(zAngle));
+                        xRotate.setAngle(Math.toDegrees(angle.x));
+                        yRotate.setAngle(Math.toDegrees(angle.y));
+                        zRotate.setAngle(Math.toDegrees(angle.z));
                     }catch (Exception e){
                         System.out.print("ROTATE ERROR\r\n");
                     }
                 }else if(me.isSecondaryButtonDown()){
-                    Rotation baseRotation = new Rotation(RotationOrder.ZXY, xAngle, yAngle, zAngle);//magic happens!
+                    Rotation baseRotation = new Rotation(RotationOrder.ZXY, angle.x, angle.y, angle.z);//magic happens!
                     Vector3D planeTranslate = baseRotation.applyInverseTo(new Vector3D(mouseDeltaX*MOVE_SCALE,mouseDeltaY*MOVE_SCALE,0));
 
-                    xOffset += planeTranslate.getX()*Math.pow(10,-scale);
-                    yOffset += planeTranslate.getY()*Math.pow(10,-scale);
-                    zOffset += planeTranslate.getZ()*Math.pow(10,-scale);
+                    offset.x += planeTranslate.getX()*Math.pow(10,-scale);
+                    offset.y += planeTranslate.getY()*Math.pow(10,-scale);
+                    offset.z += planeTranslate.getZ()*Math.pow(10,-scale);
 
                     //System.out.print("\tdx="+planeTranslate.getX()+"\tdy="+planeTranslate.getY()+"\tdz="+planeTranslate.getZ()+"\trx="+Math.toDegrees(xAngle)+"\r\n");
 
-                    xyzTranslate.setX(xOffset);
-                    xyzTranslate.setY(yOffset);
-                    xyzTranslate.setZ(zOffset);
+                    xyzTranslate.setX(offset.x);
+                    xyzTranslate.setY(offset.y);
+                    xyzTranslate.setZ(offset.z);
                 }
 
             }
