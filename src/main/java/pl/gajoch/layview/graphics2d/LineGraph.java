d8b82a4fe7d4027a21ccc292c7dbeb96d9cb671a
Piotr Rzeszut <piotrva@wp.pl>
2016-02-16 01:02:28 +0100

    2d plot dynamic color changing

--- src/main/java/pl/gajoch/layview/graphics2d/LineGraph.java
+++ src/main/java/pl/gajoch/layview/graphics2d/LineGraph.java
@@ -1,14 +1,22 @@
 package pl.gajoch.layview.graphics2d;
 
+import com.sun.javafx.geom.Vec2d;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 /**
  * Created by Piotr on 12/02/2016.
  */
 public class LineGraph extends Group {
+
+    private ArrayList<Vec2d> points;
+
     public LineGraph(){
         super();
         final NumberAxis xAxis = new NumberAxis();
@@ -16,15 +24,14 @@ public class LineGraph extends Group {
         xAxis.setLabel("Number of Month");
         //creating the chart
         final LineChart<Number,Number> lineChart =
-                new LineChart<Number,Number>(xAxis,yAxis);
-
-        lineChart.setAnimated(true);
-
-        lineChart.setTitle("Stock Monitoring, 2010");
+                new LineChart<>(xAxis,yAxis);
         //defining a series
         XYChart.Series series = new XYChart.Series();
-        series.setName("My portfolio");
+        lineChart.setLegendVisible(false);
+        //series.setName("My portfolio");
         //populating the series with data
+        //lineChart.setCreateSymbols(false);//punkty
+
         series.getData().add(new XYChart.Data(1, 23));
         series.getData().add(new XYChart.Data(2, 14));
         series.getData().add(new XYChart.Data(3, 15));
@@ -40,6 +47,18 @@ public class LineGraph extends Group {
 
         lineChart.getData().add(series);
 
+        lineChart.applyCss();
+
+        Set<Node> nodes = lineChart.lookupAll(".series" + 0);
+
+        for (Node n : nodes) {
+            StringBuilder style = new StringBuilder();
+            style.append("-fx-stroke: blue; -fx-background-color: blue, white; -fx-stroke-width: 10px;");
+            n.setStyle(style.toString());
+        }
+
         this.getChildren().add(lineChart);
+
+
     }
 }
