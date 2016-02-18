f16b62be42dd29b0132f3804ff6de19cefa07ce5
Piotr Rzeszut <piotrva@wp.pl>
2016-02-19 00:37:09 +0100

    Cleanup, 3d graphics test with JOGL

--- src/main/java/pl/gajoch/layview/graphics3d/SimpleJOGL.java
+++ src/main/java/pl/gajoch/layview/graphics3d/SimpleJOGL.java
@@ -9,38 +9,42 @@ import javax.media.opengl.*;
 import javax.media.opengl.awt.GLCanvas;
 import com.jogamp.opengl.util.*;
 
+import java.awt.DisplayMode;
+import javax.media.opengl.GL2;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.GLProfile;
+import javax.media.opengl.awt.GLCanvas;
+import javax.media.opengl.glu.GLU;
+import javax.swing.JFrame;
+
 public class SimpleJOGL implements GLEventListener {
 
-    private double theta = 0;
-    private double s = 0;
-    private double c = 0;
+    private float rquad=0.0f;
+    private GLU glu = new GLU();
+
+    public static DisplayMode dm, dm_old;
 
     public static void main(String[] args) {
-        GLProfile glp = GLProfile.getDefault();
-        GLCapabilities caps = new GLCapabilities(glp);
-        GLCanvas canvas = new GLCanvas(caps);
-
-        Frame frame = new Frame("AWT Window Test");
-        frame.setSize(300, 300);
-        frame.add(canvas);
-        frame.setVisible(true);
-
-        frame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
-
-        canvas.addGLEventListener(new SimpleJOGL());
-
-        FPSAnimator animator = new FPSAnimator(canvas, 60);
-        //animator.add(canvas);
+        final GLProfile profile = GLProfile.get( GLProfile.GL2 );
+        GLCapabilities capabilities = new GLCapabilities( profile );
+        // The canvas
+        final GLCanvas glcanvas = new GLCanvas( capabilities );
+        SimpleJOGL cube = new SimpleJOGL();
+        glcanvas.addGLEventListener( cube );
+        glcanvas.setSize( 400, 400 );
+        final JFrame frame = new JFrame ( " Multicolored cube" );
+        frame.getContentPane().add( glcanvas );
+        frame.setSize( frame.getContentPane().getPreferredSize() );
+        frame.setVisible( true );
+        final FPSAnimator animator = new FPSAnimator( glcanvas, 300,true );
         animator.start();
     }
 
     @Override
     public void display(GLAutoDrawable drawable) {
-        update();
+        //update();
         render(drawable);
     }
 
@@ -50,31 +54,75 @@ public class SimpleJOGL implements GLEventListener {
 
     @Override
     public void init(GLAutoDrawable drawable) {
+        final GL2 gl = drawable.getGL().getGL2();
+        gl.glShadeModel( GL2.GL_SMOOTH );
+        gl.glClearColor( 0f, 0f, 0f, 0f );
+        gl.glClearDepth( 1.0f );
+        gl.glEnable( GL2.GL_DEPTH_TEST );
+        gl.glDepthFunc( GL2.GL_LEQUAL );
+        gl.glHint( GL2.GL_PERSPECTIVE_CORRECTION_HINT, GL2.GL_NICEST );
     }
 
     @Override
-    public void reshape(GLAutoDrawable drawable, int x, int y, int w, int h) {
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        final GL2 gl = drawable.getGL().getGL2();
+        if( height <=0 )
+            height =1;
+        final float h = ( float ) width / ( float ) height;
+        gl.glViewport( 0, 0, width, height );
+        gl.glMatrixMode( GL2.GL_PROJECTION );
+        gl.glLoadIdentity();
+        glu.gluPerspective( 45.0f, h, 1.0, 20.0 );
+        gl.glMatrixMode( GL2.GL_MODELVIEW );
+        gl.glLoadIdentity();
     }
 
-    private void update() {
+    /*private void update() {
         theta += 0.01;
         s = Math.sin(theta);
         c = Math.cos(theta);
-    }
+    }*/
 
     private void render(GLAutoDrawable drawable) {
-        GL2 gl = drawable.getGL().getGL2();
-
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
-
-        // draw a triangle filling the window
-        gl.glBegin(GL.GL_TRIANGLES);
-        gl.glColor3f(1, 0, 0);
-        gl.glVertex2d(-c, -c);
-        gl.glColor3f(0, 1, 0);
-        gl.glVertex2d(0, c);
-        gl.glColor3f(0, 0, 1);
-        gl.glVertex2d(s, -s);
-        gl.glEnd();
+        final GL2 gl = drawable.getGL().getGL2();
+        gl.glClear( GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT );
+        gl.glLoadIdentity();
+        gl.glTranslatef( 0f, 0f, -5.0f );
+        gl.glRotatef( rquad, 1.0f, 1.0f, 1.0f ); // Rotate The Cube On X, Y & Z
+        //giving different colors to different sides
+        gl.glBegin( GL2.GL_QUADS ); // Start Drawing The Cube
+        gl.glColor3f( 1f,0f,0f );   //red color
+        gl.glVertex3f( 1.0f, 1.0f, -1.0f ); // Top Right Of The Quad (Top)
+        gl.glVertex3f( -1.0f, 1.0f, -1.0f); // Top Left Of The Quad (Top)
+        gl.glVertex3f( -1.0f, 1.0f, 1.0f ); // Bottom Left Of The Quad (Top)
+        gl.glVertex3f( 1.0f, 1.0f, 1.0f ); // Bottom Right Of The Quad (Top)
+        gl.glColor3f( 0f,1f,0f ); //green color
+        gl.glVertex3f( 1.0f, -1.0f, 1.0f ); // Top Right Of The Quad
+        gl.glVertex3f( -1.0f, -1.0f, 1.0f ); // Top Left Of The Quad
+        gl.glVertex3f( -1.0f, -1.0f, -1.0f ); // Bottom Left Of The Quad
+        gl.glVertex3f( 1.0f, -1.0f, -1.0f ); // Bottom Right Of The Quad
+        gl.glColor3f( 0f,0f,1f ); //blue color
+        gl.glVertex3f( 1.0f, 1.0f, 1.0f ); // Top Right Of The Quad (Front)
+        gl.glVertex3f( -1.0f, 1.0f, 1.0f ); // Top Left Of The Quad (Front)
+        gl.glVertex3f( -1.0f, -1.0f, 1.0f ); // Bottom Left Of The Quad
+        gl.glVertex3f( 1.0f, -1.0f, 1.0f ); // Bottom Right Of The Quad
+        gl.glColor3f( 1f,1f,0f ); //yellow (red + green)
+        gl.glVertex3f( 1.0f, -1.0f, -1.0f ); // Bottom Left Of The Quad
+        gl.glVertex3f( -1.0f, -1.0f, -1.0f ); // Bottom Right Of The Quad
+        gl.glVertex3f( -1.0f, 1.0f, -1.0f ); // Top Right Of The Quad (Back)
+        gl.glVertex3f( 1.0f, 1.0f, -1.0f ); // Top Left Of The Quad (Back)
+        gl.glColor3f( 1f,0f,1f ); //purple (red + green)
+        gl.glVertex3f( -1.0f, 1.0f, 1.0f ); // Top Right Of The Quad (Left)
+        gl.glVertex3f( -1.0f, 1.0f, -1.0f ); // Top Left Of The Quad (Left)
+        gl.glVertex3f( -1.0f, -1.0f, -1.0f ); // Bottom Left Of The Quad
+        gl.glVertex3f( -1.0f, -1.0f, 1.0f ); // Bottom Right Of The Quad
+        gl.glColor3f( 0f,1f, 1f ); //sky blue (blue +green)
+        gl.glVertex3f( 1.0f, 1.0f, -1.0f ); // Top Right Of The Quad (Right)
+        gl.glVertex3f( 1.0f, 1.0f, 1.0f ); // Top Left Of The Quad
+        gl.glVertex3f( 1.0f, -1.0f, 1.0f ); // Bottom Left Of The Quad
+        gl.glVertex3f( 1.0f, -1.0f, -1.0f ); // Bottom Right Of The Quad
+        gl.glEnd(); // Done Drawing The Quad
+        gl.glFlush();
+        rquad -=0.15f;
     }
 }
