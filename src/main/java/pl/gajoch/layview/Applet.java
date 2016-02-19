8394eafde3ecdd71fd09aa38ba539ffba82a824a
Grzegorz Gajoch <gajoch@gmail.com>
2016-02-20 00:22:36 +0100

    JOGL dependency, added 3D box example

--- src/main/java/pl/gajoch/layview/Applet.java
+++ src/main/java/pl/gajoch/layview/Applet.java
@@ -5,14 +5,19 @@ import processing.core.PApplet;
 public class Applet extends PApplet {
 
     public void setup() {
-        size(400, 400);
+        size(400, 400, P3D);
         background(0);
+        lights();
+
+        pushMatrix();
+        translate(130, height/2, 0);
+        rotateY(1.25f);
+        rotateX(-0.4f);
+        noStroke();
+        box(100);
+        popMatrix();
     }
 
     public void draw() {
-        stroke(255);
-        if (mousePressed) {
-            line(mouseX,mouseY,pmouseX,pmouseY);
-        }
     }
 }
