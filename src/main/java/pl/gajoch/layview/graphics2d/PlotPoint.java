683bceb282452bab463c22939c6b73e1666d9caa
Piotr Rzeszut <piotrva@wp.pl>
2016-03-03 01:28:15 +0100

    Plot points classes

---
+++ src/main/java/pl/gajoch/layview/graphics2d/PlotPoint.java
@@ -0,0 +1,20 @@
+package pl.gajoch.layview.graphics2d;
+
+
+import com.sun.javafx.geom.Vec2d;
+
+public class PlotPoint {
+    public Vec2d value;
+
+    public PlotPoint(){
+        value = new Vec2d();
+    }
+
+    public PlotPoint(Vec2d value){
+        this.value = value;
+    }
+
+    public PlotPoint(PlotPoint second){
+        this.value=new Vec2d(second.value);
+    }
+}
