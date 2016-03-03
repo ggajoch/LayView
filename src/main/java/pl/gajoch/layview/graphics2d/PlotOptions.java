ebc770045241b22e98e5dbfc72b95981bbd25d0d
Piotr Rzeszut <piotrva@wp.pl>
2016-03-03 01:35:26 +0100

    introducing plot options

---
+++ src/main/java/pl/gajoch/layview/graphics2d/PlotOptions.java
@@ -0,0 +1,15 @@
+package pl.gajoch.layview.graphics2d;
+
+import javafx.scene.paint.Color;
+
+public class PlotOptions {
+    public float width;
+    public boolean isLine;
+    public Color color;
+
+    public PlotOptions() {
+        width = 3;
+        isLine = true;
+        color = Color.GREEN;
+    }
+}
---
+++ src/main/java/pl/gajoch/layview/graphics2d/PlotPresenter.java
@@ -0,0 +1,5 @@
+package pl.gajoch.layview.graphics2d;
+
+public class PlotPresenter {
+    public PlotOptions options;
+}
