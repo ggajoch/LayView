e237c5403edc30ea5ea401a5ec99a122d2f79311
Grzegorz Gajoch <gajoch@gmail.com>
2016-03-21 00:24:59 +0100

    Priority queue refactor

--- src/main/java/pl/gajoch/layview/scheduler/Scheduler.java
+++ src/main/java/pl/gajoch/layview/scheduler/Scheduler.java
@@ -1,65 +1,50 @@
 package pl.gajoch.layview.scheduler;
 
 import java.util.*;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class Scheduler {
-    private static final PriorityQueue<Event> queue = new PriorityQueue<>();
-    private static final PriorityQueue<Event> queueCopy = new PriorityQueue<>();
-
-    private static Object mutex = new Object();
+    private static final PriorityBlockingQueue<Event> queue = new PriorityBlockingQueue<>();
+    private static final PriorityBlockingQueue<Event> queueCopy = new PriorityBlockingQueue<>();
 
     private Scheduler() {
     }
 
     public static void start() {
-        synchronized (mutex) {
-            queueCopy.clear();
-            queueCopy.addAll(queue);
+        queueCopy.clear();
+        queueCopy.addAll(queue);
 
-            for (Event event : queue) {
-                event.resetHandler();
-            }
+        for (Event event : queue) {
+            event.resetHandler();
         }
 
         long start = micros();
         while (!queueCopy.isEmpty()) {
             Event now;
             long next;
-            synchronized (mutex) {
-                now = queueCopy.peek();
-                next = start + now.offset_actual;
-            }
+            now = queueCopy.peek();
+            next = start + now.offset_actual;
             while (micros() < next) {
             }
-            synchronized (mutex) {
-                now.dispatchHandler();
-                queueCopy.remove();
-            }
+            now.dispatchHandler();
+            queueCopy.remove();
         }
     }
 
     public static void remove(Event event) {
-        synchronized (mutex) {
-            queue.remove(event);
-        }
+        queue.remove(event);
     }
 
     static Boolean isRunning() {
-        synchronized (mutex) {
-            return queueCopy.isEmpty();
-        }
+        return queueCopy.isEmpty();
     }
 
     public static void schedule(Event event) {
-        synchronized (mutex) {
-            queue.add(event);
-        }
+        queue.add(event);
     }
 
     public static void __scheduleCurrentRun(Event event) {
-        synchronized (mutex) {
-            queueCopy.add(event);
-        }
+        queueCopy.add(event);
     }
 
     private static long micros() {
